33b68a836590a62c4e69d7c620e4a8c6c3fb629c
Alexander Lamaison <awl03@doc.ic.ac.uk>
2009-01-01 23:48:13 +0000

    Moved CGlobal lock to DataObject.h as it is a generic DataObject helper. Moved CFileGroupDescriptor to DataObject.h to prevent circular reference but it will probably be moved back when non-generic functionality is refactored out of CDataObject.

--- ShellDataObject.h
+++ ShellDataObject.h
@@ -22,59 +22,6 @@
 #include "DataObject.h"
 #include "Pidl.h"
 
-class CGlobalLock
-{
-public:
-	CGlobalLock() throw() :
-		m_hGlobal(NULL), m_pMem(NULL)
-	{}
-	CGlobalLock(__in HGLOBAL hGlobal) throw() : 
-		m_hGlobal(hGlobal), m_pMem(::GlobalLock(m_hGlobal))
-	{}
-
-	~CGlobalLock() throw()
-	{
-		Clear();
-	}
-
-	/**
-	 * Disable copy constructor.  If the object were copied, the old one would
-	 * be destroyed which unlocks the global memory but the new copy would
-	 * not be re-locked.
-	 */
-	CGlobalLock(const CGlobalLock& lock) throw();
-	CGlobalLock& operator=(const CGlobalLock&) throw();
-
-	void Attach(__in HGLOBAL hGlobal) throw()
-	{
-		Clear();
-
-		m_hGlobal = hGlobal;
-		m_pMem = ::GlobalLock(m_hGlobal);
-	}
-
-	void Clear() throw()
-	{
-		m_pMem = NULL;
-		if (m_hGlobal)
-			::GlobalUnlock(m_hGlobal);
-	}
-
-	CIDA* GetCida()
-	{
-		return static_cast<CIDA *>(m_pMem);
-	}
-
-	FILEGROUPDESCRIPTOR& GetFileGroupDescriptor()
-	{
-		return *static_cast<FILEGROUPDESCRIPTOR*>(m_pMem);
-	}
-
-private:
-	HGLOBAL m_hGlobal;
-	PVOID m_pMem;
-};
-
 class CShellDataObject
 {
 public:
@@ -90,54 +37,4 @@ private:
 	CComPtr<IDataObject> m_spDataObj;
 	CStorageMedium m_medium;
 	CGlobalLock m_glock;
-};
-
-class CFileGroupDescriptor
-{
-public:
-	CFileGroupDescriptor(UINT cFiles)
-		: m_hGlobal(NULL)
-	{
-		ATLENSURE_THROW(cFiles > 0, E_INVALIDARG);
-
-		// Allocate global memory sufficient for group descriptor and as many
-		// file descriptors as specified
-		size_t cbData = sizeof FILEGROUPDESCRIPTOR + 
-		                sizeof FILEDESCRIPTOR * (cFiles - 1);
-		m_hGlobal = ::GlobalAlloc(GMEM_MOVEABLE, cbData);
-		ATLENSURE_THROW(m_hGlobal, E_OUTOFMEMORY);
-
-		// Zero the entire block
-		CGlobalLock glock(m_hGlobal);
-		FILEGROUPDESCRIPTOR& fgd = glock.GetFileGroupDescriptor();
-		::ZeroMemory(&fgd, cbData);
-		fgd.cItems = cFiles;
-	}
-	
-	~CFileGroupDescriptor()
-	{
-		::GlobalFree(m_hGlobal);
-		m_hGlobal = NULL;
-	}
-
-	void SetDescriptor(UINT i, FILEDESCRIPTOR& fd)
-	{
-		CGlobalLock glock(m_hGlobal);
-
-		FILEGROUPDESCRIPTOR& fgd = glock.GetFileGroupDescriptor();
-		if (i >= fgd.cItems)
-			AtlThrow(E_INVALIDARG); // Out-of-range
-
-		::CopyMemory(&(fgd.fgd[i]), &fd, sizeof fd);
-	}
-
-	HGLOBAL Detach()
-	{
-		HGLOBAL hGlobal = m_hGlobal;
-		m_hGlobal = NULL;
-		return hGlobal;
-	}
-
-private:
-	HGLOBAL m_hGlobal;
 };
