5059376069772fa81c4d73521d9a9c8df6f0bb5a
Alexander Lamaison <alexander.lamaison@gmail.com>
2015-10-05 21:11:26 +0100

    Started SFTP path class.

---
+++ ssh/path.hpp
@@ -0,0 +1,246 @@
+/**
+    @file
+
+    SSH SFTP path.
+
+    @if license
+
+    Copyright (C) 2015  Alexander Lamaison <awl03@doc.ic.ac.uk>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+    In addition, as a special exception, the the copyright holders give you
+    permission to combine this program with free software programs or the
+    OpenSSL project's "OpenSSL" library (or with modified versions of it,
+    with unchanged license). You may copy and distribute such a system
+    following the terms of the GNU GPL for this program and the licenses
+    of the other code concerned. The GNU General Public License gives
+    permission to release a modified version without this exception; this
+    exception also makes it possible to release a modified version which
+    carries forward this exception.
+
+    @endif
+*/
+
+#ifndef SSH_PATH_HPP
+#define SSH_PATH_HPP
+
+#include <boost/iterator/transform_iterator.hpp>
+#include <boost/operators.hpp>
+#include <boost/token_iterator.hpp> // token_iterator_generator, char_separator
+
+#include <algorithm> // lexicographical_compare
+#include <ostream>
+#include <string>
+#include <utility> // swap
+
+namespace ssh {
+namespace filesystem {
+
+namespace detail {
+
+template<typename Value>
+class all_but_first_slash_separator
+{
+public:
+    all_but_first_slash_separator() :
+        m_at_beginning(true), m_normal_separator("/") {}
+
+    void reset()
+    {
+        all_but_first_slash_separator<Value> fresh;
+        std::swap(*this, fresh);
+    }
+
+    template<typename InputIterator, typename Token>
+    bool operator()(InputIterator& next, InputIterator& end, Token& token_out)
+    {
+        if (m_at_beginning)
+        {
+            m_at_beginning = false;
+            if (next != end && *next == '/')
+            {
+                token_out = "/";
+                return true;
+            }
+            else
+            {
+                return m_normal_separator(next, end, token_out);
+            }
+        }
+        else
+        {
+            return m_normal_separator(next, end, token_out);
+        }
+    }
+
+private:
+    boost::char_separator<Value> m_normal_separator;
+    bool m_at_beginning;
+};
+
+
+// Because the iterators produce paths, std::lexicograpical_compare will not
+// terminate. Therefore we have our own that works on the string form of the
+// path in each segment
+template<typename Iterator>
+inline int lexical_compare(
+    Iterator lhs, const Iterator& lhs_end,
+    Iterator rhs, const Iterator& rhs_end)
+{
+    while (lhs != lhs_end && rhs != rhs_end)
+    {
+        int comparison = lhs->native().compare(rhs->native());
+        if (comparison == 0)
+        {
+            ++lhs;
+            ++rhs;
+            continue;
+        }
+        else
+        {
+            return comparison;
+        }
+    }
+
+    if (lhs != lhs_end)
+    {
+        return 1;
+    }
+    else if (rhs != rhs_end)
+    {
+        return -1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+}
+
+
+class path : boost::totally_ordered<path>
+{
+public:
+    typedef std::string string_type;
+    typedef string_type::value_type value_type;
+
+private:
+    typedef detail::all_but_first_slash_separator<value_type> separator_type_;
+
+    static const separator_type_& segment_separator()
+    {
+        static separator_type_ separator;
+        return separator;
+    }
+
+    typedef boost::token_iterator_generator<separator_type_>::type token_iterator;
+
+    struct path_factory
+    {
+        typedef path result_type;
+        result_type operator()(const token_iterator::value_type& source) const
+        {
+            return result_type(source);
+        }
+    };
+
+    token_iterator begin_tokens() const
+    {
+        return boost::make_token_iterator<string_type>(
+            m_path.begin(), m_path.end(), segment_separator());
+    }
+
+    token_iterator end_tokens() const
+    {
+        return boost::make_token_iterator<string_type>(
+            m_path.end(), m_path.end(), segment_separator());
+    }
+
+public:
+    typedef boost::transform_iterator<path_factory, token_iterator> iterator;
+
+private:
+
+public:
+
+    path() {}
+    explicit path(const string_type& source) : m_path(source) {}
+
+    bool is_relative() const
+    {
+        return !is_absolute();
+    }
+
+    bool is_absolute() const
+    {
+        return !empty() && m_path[0] == '/';
+    }
+
+    bool empty() const
+    {
+        return m_path.empty();
+    }
+
+    string_type native() const
+    {
+        return m_path;
+    }
+
+    operator string_type() const
+    {
+        return native();
+    }
+
+    int compare(const path& rhs) const
+    {
+        return detail::lexical_compare(begin(), end(), rhs.begin(), rhs.end());
+    }
+
+    iterator begin() const
+    {
+        return iterator(begin_tokens());
+    }
+
+    iterator end() const
+    {
+        return iterator(end_tokens());
+    }
+
+private:
+    string_type m_path;
+};
+
+std::ostream& operator<<(std::ostream& stream, const path& p)
+{
+    // TODO: quote string so it can roundtrip
+    stream << p.native();
+    return stream;
+}
+
+bool operator==(const path& lhs, const path& rhs)
+{
+    return lhs.compare(rhs) == 0;
+}
+
+bool operator<(const path& lhs, const path& rhs)
+{
+    return lhs.compare(rhs) < 0;
+}
+
+}} // namespace ssh::filesystem
+
+#endif
---
+++ test/ssh/path_test.cpp
@@ -0,0 +1,437 @@
+/**
+    @file
+
+    Tests for ssh filesystem path.
+
+    @if license
+
+    Copyright (C) 2010, 2011, 2013, 2015
+    Alexander Lamaison <awl03@doc.ic.ac.uk>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+    In addition, as a special exception, the the copyright holders give you
+    permission to combine this program with free software programs or the
+    OpenSSL project's "OpenSSL" library (or with modified versions of it,
+    with unchanged license). You may copy and distribute such a system
+    following the terms of the GNU GPL for this program and the licenses
+    of the other code concerned. The GNU General Public License gives
+    permission to release a modified version without this exception; this
+    exception also makes it possible to release a modified version which
+    carries forward this exception.
+
+    @endif
+*/
+
+#include "ssh/path.hpp"
+
+#include <boost/test/unit_test.hpp>
+
+using ssh::filesystem::path;
+
+BOOST_AUTO_TEST_SUITE(path_tests)
+
+BOOST_AUTO_TEST_CASE( default_path_is_empty )
+{
+    const path p;
+    BOOST_CHECK(p.empty());
+}
+
+BOOST_AUTO_TEST_CASE( default_path_is_equal_to_itself )
+{
+    const path p;
+    BOOST_CHECK_EQUAL(p, p);
+}
+
+BOOST_AUTO_TEST_CASE( default_path_is_equal_to_another_default_path )
+{
+    const path p;
+    const path q;
+    BOOST_CHECK_EQUAL(p, q);
+}
+
+BOOST_AUTO_TEST_CASE( default_path_is_equal_to_a_constructed_copy )
+{
+    const path p;
+    const path q(p);
+    BOOST_CHECK_EQUAL(p, q);
+}
+
+BOOST_AUTO_TEST_CASE( default_path_is_equal_to_an_assigned_copy )
+{
+    const path p;
+    path q;
+    q = p;
+    BOOST_CHECK_EQUAL(p, q);
+}
+
+BOOST_AUTO_TEST_CASE( default_path_is_different_to_a_single_segment_path )
+{
+    const path p;
+    const path q("other path");
+    BOOST_CHECK_NE(p, q);
+}
+
+BOOST_AUTO_TEST_CASE( default_path_converts_explicity_to_empty_string )
+{
+    const path p;
+    BOOST_CHECK_EQUAL(p.native(), "");
+}
+
+BOOST_AUTO_TEST_CASE( default_path_converts_implicitly_to_empty_string )
+{
+    const path p;
+    const path::string_type s = p;
+    BOOST_CHECK_EQUAL(s, "");
+}
+
+BOOST_AUTO_TEST_CASE( default_path_is_at_end_of_iteration )
+{
+    const path p;
+    BOOST_CHECK(p.begin() == p.end());
+    BOOST_CHECK_EQUAL(std::distance(p.begin(), p.end()), 0);
+}
+
+BOOST_AUTO_TEST_CASE( default_path_is_relative )
+{
+    const path p;
+    BOOST_CHECK(p.is_relative());
+}
+
+BOOST_AUTO_TEST_CASE( default_path_is_not_absolute )
+{
+    const path p;
+    BOOST_CHECK(!p.is_absolute());
+}
+
+BOOST_AUTO_TEST_CASE( root_path_is_not_empty )
+{
+    const path p("/");
+    BOOST_CHECK(!p.empty());
+}
+
+BOOST_AUTO_TEST_CASE( root_path_is_equal_to_itself )
+{
+    const path p("/");
+    BOOST_CHECK_EQUAL(p, p);
+}
+
+BOOST_AUTO_TEST_CASE( root_path_is_equal_to_another_root_path )
+{
+    const path p("/");
+    const path q("/");
+    BOOST_CHECK_EQUAL(p, q);
+}
+
+BOOST_AUTO_TEST_CASE( root_path_is_different_to_a_non_root_relative_path )
+{
+    const path p("/");
+    const path q("foo");
+    BOOST_CHECK_NE(p, q);
+}
+
+BOOST_AUTO_TEST_CASE( root_path_is_different_to_a_non_root_absolute_path )
+{
+    const path p("/");
+    const path q("/foo");
+    BOOST_CHECK_NE(p, q);
+}
+
+BOOST_AUTO_TEST_CASE( root_path_is_different_to_a_default_path )
+{
+    const path p("/");
+    const path q;
+    BOOST_CHECK_NE(p, q);
+}
+
+BOOST_AUTO_TEST_CASE( root_path_is_equal_to_a_constructed_copy )
+{
+    const path p("/");
+    const path q(p);
+    BOOST_CHECK_EQUAL(p, q);
+}
+
+BOOST_AUTO_TEST_CASE( root_path_is_equal_to_an_assigned_copy )
+{
+    const path p("/");
+    path q;
+    q = p;
+    BOOST_CHECK_EQUAL(p, q);
+}
+
+BOOST_AUTO_TEST_CASE( root_path_converts_explicity_to_original_string )
+{
+    const path p("/");
+    BOOST_CHECK_EQUAL(p.native(), "/");
+}
+
+BOOST_AUTO_TEST_CASE( root_path_converts_implicitly_to_original_string )
+{
+    const path p("/");
+    const path::string_type s = p;
+    BOOST_CHECK_EQUAL(s, "/");
+}
+
+BOOST_AUTO_TEST_CASE( root_path_can_iterate_once )
+{
+    const path p("/");
+    BOOST_CHECK(p.begin() != p.end());
+    BOOST_CHECK_EQUAL(std::distance(p.begin(), p.end()), 1);
+}
+
+BOOST_AUTO_TEST_CASE( root_path_iterator_produces_original_path )
+{
+    const path p("/");
+    BOOST_REQUIRE(p.begin() != p.end());
+    BOOST_CHECK_EQUAL(*(p.begin()), path("/"));
+}
+
+BOOST_AUTO_TEST_CASE( root_path_is_not_relative )
+{
+    const path p("/");
+    BOOST_CHECK(!p.is_relative());
+}
+
+BOOST_AUTO_TEST_CASE( root_path_is_absolute )
+{
+    const path p("/");
+    BOOST_CHECK(p.is_absolute());
+}
+
+BOOST_AUTO_TEST_CASE( single_segment_absolute_path_is_not_empty )
+{
+    const path p("/Test Filename.txt");
+    BOOST_CHECK(!p.empty());
+}
+
+BOOST_AUTO_TEST_CASE( single_segment_absolute_path_is_equal_to_itself )
+{
+    const path p("/Test Filename.txt");
+    BOOST_CHECK_EQUAL(p, p);
+}
+
+BOOST_AUTO_TEST_CASE( single_segment_absolute_path_is_equal_to_another_path_from_equal_source )
+{
+    const path p("/Test Filename.txt");
+    const path q("/Test Filename.txt");
+    BOOST_CHECK_EQUAL(p, q);
+}
+
+BOOST_AUTO_TEST_CASE( single_segment_absolute_path_is_different_to_another_path_from_different_source )
+{
+    const path p("/Test Filename.txt");
+    const path q("/Test Filename.txp");
+    BOOST_CHECK_NE(p, q);
+}
+
+BOOST_AUTO_TEST_CASE( single_segment_absolute_path_is_different_to_similar_relative_path )
+{
+    const path p("/Test Filename.txt");
+    const path q("Test Filename.txt");
+    BOOST_CHECK_NE(p, q);
+}
+
+BOOST_AUTO_TEST_CASE( single_segment_absolute_path_equality_is_case_sensitive )
+{
+    const path p("/Test Filename.txt");
+    const path q("/Test filename.txt");
+    BOOST_CHECK_NE(p, q);
+}
+
+BOOST_AUTO_TEST_CASE( single_segment_absolute_path_is_equal_to_a_constructed_copy )
+{
+    const path p("/Test Filename.txt");
+    const path q(p);
+    BOOST_CHECK_EQUAL(p, q);
+}
+
+BOOST_AUTO_TEST_CASE( single_segment_absolute_path_is_equal_to_an_assigned_copy )
+{
+    const path p("/Test Filename.txt");
+    path q;
+    q = p;
+    BOOST_CHECK_EQUAL(p, q);
+}
+
+BOOST_AUTO_TEST_CASE( single_segment_absolute_path_is_less_than_lexi_greater_source )
+{
+    const path p("/Test Filename.txs");
+    const path q("/Test Filename.txt");
+    BOOST_CHECK_LT(p, q);
+}
+
+BOOST_AUTO_TEST_CASE( single_segment_absolute_path_is_greater_than_lexi_less_source )
+{
+    const path p("/Test Filename.txt");
+    const path q("/Test Filename.txs");
+    BOOST_CHECK_GT(p, q);
+}
+
+BOOST_AUTO_TEST_CASE( single_segment_absolute_path_converts_explicity_to_original_string )
+{
+    const path p("/Test Filename.txt");
+    BOOST_CHECK_EQUAL(p.native(), "/Test Filename.txt");
+}
+
+BOOST_AUTO_TEST_CASE( single_segment_absolute_path_converts_implicitly_to_original_string )
+{
+    const path p("/Test Filename.txt");
+    const path::string_type s = p;
+    BOOST_CHECK_EQUAL(s, "/Test Filename.txt");
+}
+
+BOOST_AUTO_TEST_CASE( single_segment_absolute_path_can_iterate_twice )
+{
+    const path p("/Test Filename.txt");
+    BOOST_CHECK(p.begin() != p.end());
+    BOOST_CHECK_EQUAL(std::distance(p.begin(), p.end()), 2);
+}
+
+BOOST_AUTO_TEST_CASE( single_segment_absolute_path_iterator_produces_root_and_filename_single_segment_paths )
+{
+    const path p("/Test Filename.txt");
+    BOOST_REQUIRE(p.begin() != p.end());
+
+    path::iterator it = p.begin();
+    BOOST_CHECK_EQUAL(*it++, path("/"));
+    BOOST_CHECK_EQUAL(*it++, path("Test Filename.txt"));
+    BOOST_CHECK(it == p.end());
+}
+
+BOOST_AUTO_TEST_CASE( single_segment_absolute_path_is_not_relative )
+{
+    const path p("/Test Filename.txt");
+    BOOST_CHECK(!p.is_relative());
+}
+
+BOOST_AUTO_TEST_CASE( single_segment_absolute_path_is_absolute )
+{
+    const path p("/Test Filename.txt");
+    BOOST_CHECK(p.is_absolute());
+}
+
+BOOST_AUTO_TEST_CASE( multi_segment_relative_path_is_not_empty )
+{
+    const path p("Test Dir/Test Filename.txt");
+    BOOST_CHECK(!p.empty());
+}
+
+BOOST_AUTO_TEST_CASE( multi_segment_relative_path_is_equal_to_itself )
+{
+    const path p("Test Dir/Test Filename.txt");
+    BOOST_CHECK_EQUAL(p, p);
+}
+
+BOOST_AUTO_TEST_CASE( multi_segment_relative_path_is_equal_to_another_path_from_equal_source )
+{
+    const path p("Test Dir/Test Filename.txt");
+    const path q("Test Dir/Test Filename.txt");
+    BOOST_CHECK_EQUAL(p, q);
+}
+
+BOOST_AUTO_TEST_CASE( multi_segment_relative_path_is_different_to_another_path_with_same_dir_different_file )
+{
+    const path p("Test Dir/Test Filename.txt");
+    const path q("Test Dir/Test Filename.txp");
+    BOOST_CHECK_NE(p, q);
+}
+
+BOOST_AUTO_TEST_CASE( multi_segment_relative_path_is_different_to_another_path_with_different_dir_same_file )
+{
+    const path p("Test Dir/Test Filename.txt");
+    const path q("Test Dir 2/Test Filename.txt");
+    BOOST_CHECK_NE(p, q);
+}
+
+BOOST_AUTO_TEST_CASE( multi_segment_relative_path_equality_is_case_sensitive )
+{
+    const path p("Test Dir/Test Filename.txt");
+    const path q("Test Dir/Test filename.txt");
+    BOOST_CHECK_NE(p, q);
+}
+
+BOOST_AUTO_TEST_CASE( multi_segment_relative_path_is_equal_to_a_constructed_copy )
+{
+    const path p("Test Dir/Test Filename.txt");
+    const path q(p);
+    BOOST_CHECK_EQUAL(p, q);
+}
+
+BOOST_AUTO_TEST_CASE( multi_segment_relative_path_is_equal_to_an_assigned_copy )
+{
+    const path p("Test Dir/Test Filename.txt");
+    path q;
+    q = p;
+    BOOST_CHECK_EQUAL(p, q);
+}
+
+BOOST_AUTO_TEST_CASE( multi_segment_relative_path_compares_less_than_lexi_by_segment )
+{
+    const path p("a/ad");
+    const path q("a+/c");
+    BOOST_CHECK_LT(p, q);
+}
+
+BOOST_AUTO_TEST_CASE( multi_segment_relative_path_compares_greater_than_lexi_by_segment )
+{
+    const path p("a+/c");
+    const path q("a/ad");
+    BOOST_CHECK_GT(p, q);
+}
+
+BOOST_AUTO_TEST_CASE( multi_segment_relative_path_converts_explicity_to_original_string )
+{
+    const path p("Test Dir/Test Filename.txt");
+    BOOST_CHECK_EQUAL(p.native(), "Test Dir/Test Filename.txt");
+}
+
+BOOST_AUTO_TEST_CASE( multi_segment_relative_path_converts_implicitly_to_original_string )
+{
+    const path p("Test Dir/Test Filename.txt");
+    const path::string_type s = p;
+    BOOST_CHECK_EQUAL(s, "Test Dir/Test Filename.txt");
+}
+
+BOOST_AUTO_TEST_CASE( multi_segment_relative_path_can_iterate_twice )
+{
+    const path p("Test Dir/Test Filename.txt");
+    BOOST_CHECK(p.begin() != p.end());
+    BOOST_CHECK_EQUAL(std::distance(p.begin(), p.end()), 2);
+}
+
+BOOST_AUTO_TEST_CASE( multi_segment_relative_path_iterator_produces_dir_and_file_single_segment_paths )
+{
+    const path p("Test Dir/Test Filename.txt");
+    BOOST_REQUIRE(p.begin() != p.end());
+
+    path::iterator it = p.begin();
+    BOOST_CHECK_EQUAL(*it++, path("Test Dir"));
+    BOOST_CHECK_EQUAL(*it++, path("Test Filename.txt"));
+    BOOST_CHECK(it == p.end());
+}
+
+BOOST_AUTO_TEST_CASE( multi_segment_relative_path_is_relative )
+{
+    const path p("Test Dir/Test Filename.txt");
+    BOOST_CHECK(p.is_relative());
+}
+
+BOOST_AUTO_TEST_CASE( multi_segment_relative_path_is_not_absolute )
+{
+    const path p("Test Dir/Test Filename.txt");
+    BOOST_CHECK(!p.is_absolute());
+}
+
+BOOST_AUTO_TEST_SUITE_END();
