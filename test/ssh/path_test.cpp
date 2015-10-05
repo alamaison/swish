293530b8ebebf7b8970057609b5f7de791d2a71e
Alexander Lamaison <alexander.lamaison@gmail.com>
2015-10-05 21:34:39 +0100

    Fix copyright date.

--- test/ssh/path_test.cpp
+++ test/ssh/path_test.cpp
@@ -5,8 +5,7 @@
 
     @if license
 
-    Copyright (C) 2010, 2011, 2013, 2015
-    Alexander Lamaison <awl03@doc.ic.ac.uk>
+    Copyright (C) 2015 Alexander Lamaison <awl03@doc.ic.ac.uk>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
