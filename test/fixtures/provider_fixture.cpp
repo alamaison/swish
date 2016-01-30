2b1fd06533ac07b8391a12b40c12706698cb0b9b
Alexander Lamaison <alexander.lamaison@gmail.com>
2016-01-30 22:40:47 +0000

    Dockerise shell_folder tests.

--- test/fixtures/provider_fixture.cpp
+++ test/fixtures/provider_fixture.cpp
@@ -19,10 +19,17 @@
 
 #include "swish/connection/connection_spec.hpp"
 #include "swish/connection/session_manager.hpp"
-#include "swish/provider/Provider.hpp" // CProvider
+#include "swish/provider/Provider.hpp"         // CProvider
+#include "swish/host_folder/host_pidl.hpp"     // create_host_itemid
+#include "swish/shell_folder/SftpDataObject.h" // CSftpDataObject
+#include "swish/shell_folder/SftpDirectory.h"  // CSftpDirectory
 
 #include <comet/ptr.h> // com_ptr
 
+#include <washer/shell/pidl_array.hpp> // PIDL array wrapper
+#include <washer/shell/shell.hpp>      // desktop_folder
+
+#include <boost/numeric/conversion/cast.hpp> // numeric_cast
 #include <boost/shared_ptr.hpp>
 
 #include <string>
@@ -30,12 +37,35 @@
 using swish::connection::connection_spec;
 using swish::connection::session_manager;
 using swish::connection::session_reservation;
-using swish::provider::sftp_provider;
+using swish::host_folder::create_host_itemid;
 using swish::provider::CProvider;
+using swish::provider::sftp_provider;
 
 using comet::com_ptr;
 
+using namespace washer::shell::pidl;
+using washer::shell::desktop_folder;
+
+using boost::numeric_cast;
 using boost::shared_ptr;
+
+using std::vector;
+
+using ssh::filesystem::path;
+
+namespace comet
+{
+
+template <>
+struct comtype<IDataObject>
+{
+    static const IID& uuid()
+    {
+        return IID_IDataObject;
+    }
+    typedef ::IUnknown base;
+};
+}
 
 namespace test
 {
@@ -57,6 +87,46 @@ com_ptr<test::MockConsumer> provider_fixture::Consumer()
     consumer->set_key_files(private_key_path().string(),
                             public_key_path().string());
     return consumer;
+}
+
+apidl_t provider_fixture::directory_pidl(const path& directory)
+{
+    return fake_swish_pidl() +
+           create_host_itemid(whost(), wuser(), directory, port());
+}
+
+apidl_t provider_fixture::sandbox_pidl()
+{
+    return directory_pidl(sandbox());
+}
+
+vector<cpidl_t> provider_fixture::pidls_in_sandbox()
+{
+    CSftpDirectory dir(sandbox_pidl(), Provider());
+    com_ptr<IEnumIDList> pidl_enum = dir.GetEnum(
+        SHCONTF_FOLDERS | SHCONTF_NONFOLDERS | SHCONTF_INCLUDEHIDDEN);
+
+    vector<cpidl_t> pidls;
+    cpidl_t pidl;
+    while (pidl_enum->Next(1, pidl.out(), NULL) == S_OK)
+    {
+        pidls.push_back(pidl);
+    }
+
+    return pidls;
+}
+
+com_ptr<IDataObject> provider_fixture::data_object_from_sandbox()
+{
+    vector<cpidl_t> pidls = pidls_in_sandbox();
+    pidl_array<cpidl_t> array(pidls.begin(), pidls.end());
+    BOOST_REQUIRE_EQUAL(array.size(), 2U);
+
+    com_ptr<IDataObject> data_object =
+        new CSftpDataObject(numeric_cast<UINT>(array.size()), array.as_array(),
+                            sandbox_pidl().get(), Provider());
+    BOOST_REQUIRE(data_object);
+    return data_object;
 }
 }
 } // namespace test::fixture
--- test/fixtures/provider_fixture.hpp
+++ test/fixtures/provider_fixture.hpp
@@ -17,9 +17,14 @@
 #define SWISH_TEST_FIXTURES_PROVIDER_FIXTURE_HPP
 
 #include "test/common_boost/MockConsumer.hpp"
-#include "test/fixtures/openssh_fixture.hpp"
+#include "test/common_boost/SwishPidlFixture.hpp"
+#include "test/fixtures/sftp_fixture.hpp"
 
 #include "swish/provider/sftp_provider.hpp"
+
+#include <ssh/filesystem/path.hpp>
+
+#include <washer/shell/pidl.hpp> // apidl_t, cpidl_t
 
 #include <comet/ptr.h> // com_ptr
 
@@ -28,14 +33,9 @@
 
 #include <string>
 
-namespace swish
-{
-namespace connection
-{
+#include <vector>
 
-class authenticated_session;
-}
-}
+#include <ObjIdl.h> // IDataObject
 
 namespace test
 {
@@ -43,7 +43,8 @@ namespace fixtures
 {
 
 /** Fixture for tests that need a backend data provider. */
-class provider_fixture : virtual public openssh_fixture
+class provider_fixture : virtual public sftp_fixture,
+                         public test::SwishPidlFixture // for fake_swish_pidl
 {
 public:
     /**
@@ -55,6 +56,34 @@ public:
      * Get a dummy consumer to use in calls to provider.
      */
     comet::com_ptr<test::MockConsumer> Consumer();
+
+    /**
+     * Return an absolute PIDL to a remote directory.
+     *
+     * We cheat by returning a PIDL to a HostFolder item with the
+     * shortcut path set to the remote directory.
+     */
+    washer::shell::pidl::apidl_t
+    directory_pidl(const ssh::filesystem::path& directory);
+
+    /**
+     * Return an absolute PIDL to the sandbox on the remote end.
+     *
+     * This is, of course, the local sandbox but the PIDL points to
+     * it via Swish rather than via the local filesystem.
+     */
+    washer::shell::pidl::apidl_t sandbox_pidl();
+
+    /**
+     * Return pidls for all the items in the sandbox directory.
+     */
+    std::vector<washer::shell::pidl::cpidl_t> pidls_in_sandbox();
+
+    /**
+     * Make a DataObject to all the items in the sandbox, via the SFTP
+     * connection.
+     */
+    comet::com_ptr<IDataObject> data_object_from_sandbox();
 };
 }
 } // namespace test::fixture
